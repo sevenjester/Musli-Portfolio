f0a7f862ba718676b51934bb0e2f0aa24e801e31
sevenjester <sevenjester1@gmail.com>
2024-06-20 14:13:34 +0800

    'src'

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import reactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App.jsx';
 
 reactDOM.createRoot(document.getElementById('root')).render(<App />);
