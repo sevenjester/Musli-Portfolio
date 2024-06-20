12bf80681c4298f1692005f8b1bb5373185b2be0
sevenjester <sevenjester1@gmail.com>
2024-06-20 18:07:50 +0800

    src

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.glb'],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  },
- build: {
-    rollupOptions: {
-      external: ['./src/main.jsx']
-    }
-  }
 })
